176f44e2abf1f8617226e9f5f05e8a2351234da3
Abraham <abrahamnhier97@gmail.com>
2024-11-25 16:22:36 +0300

    prologue section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Features from '../components/Features';
+import Story from '../components/Story';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
     </div>
   )
 }
